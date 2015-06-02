f3eb5d9093f541ba959d718be3412e18db7eb7fe
Khalaf Abdifatah <khalaf.abdifatah@hotmail.com>
2015-06-03 01:16:39 +0200

    removed debuging comment

--- src/SelectionCanvas.java
+++ src/SelectionCanvas.java
@@ -5,7 +5,6 @@ public class SelectionCanvas extends JComponent{
     private int x, y, width, height;
 
     public void paint(Graphics g) {
-        System.out.println("drawing x:" + x + ", y: " + y + ", width: " + width + ", height: " + height);
         g.drawRect(x, y, width, height);
     }
 
